07780875b3335d557ff981264ae7f90e2c2d9ceb
hashtegui <guilherme.castro.souza1999@gmail.com>
2022-01-04 21:26:00 -0400

    atualizar redirecionamento

--- jogoteca.py
+++ jogoteca.py
@@ -34,6 +34,8 @@ def index():
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login?proxima=novo')
     return render_template('novo.html', titulo='Novo Jogo')
 
 @app.route('/criar', methods=['POST',])
@@ -49,18 +51,25 @@ def criar():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
 
 @app.route('/autenticar', methods=['POST',])
 def autenticar():
     if 'mestra' == request.form['senha']:
         session['usuario_logado'] = request.form['usuario']
         flash(request.form['usuario'] + ' logou com sucesso!')
-        return redirect('/')
+        proxima_pagina = request.form['proxima']
+        return redirect('/{}'.format(proxima_pagina))
     else:
         flash('Não logado, tente novamente')
         return redirect('/login')
 
+@app.route('/logout')
+def logout():
+    session['usuario_logado']=None
+    flash('Nenhum usuário logado!')
+    return redirect('/')
 
 app.run(debug=True)
 
